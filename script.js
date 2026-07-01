f97e644a2e8dd31da63d792f8c3a6b6f0f52e90e
arsalaan120 <ibrahim.arsalaan.21@gmail.com>
2026-07-01 14:30:55 +0530

    Updated invitation design

--- script.js
+++
@@ -1,7 +0,0 @@
-const music = document.getElementById("song");
-
-window.addEventListener("load", () => {
-    music.play().catch(() => {
-        console.log("Autoplay blocked by browser.");
-    });
-});
